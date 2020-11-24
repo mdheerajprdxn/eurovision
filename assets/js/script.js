5d75ebb46ebabdc45369aef0f79f42a8308b29d9
mdheerajprdxn <dheerajm@prdxn.com>
2020-11-24 10:53:41 +0000

    [js] form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,29 @@
-/* Author: 
+let myForm = document.querySelector("#form");
+myForm.addEventListener("submit", validator);
 
-*/
+let firstName = document.querySelector("#first-name");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function validator(e) {
+  e.preventDefault();
+  let newsLetter = null;
+  if (e.target.yes.checked) {
+    newsLetter = true;
+  } else if (e.target.no.checked) {
+    newsLetter = false;
+  }
+  if (
+    e.target.firstName.value == "" ||
+    e.target.lastName.value == "" ||
+    e.target.position.value == "" ||
+    e.target.company.value == "" ||
+    e.target.companyType.value == "select" ||
+    e.target.country.value == "choose" ||
+    e.target.email.value == "" ||
+    newsLetter == null
+  ) {
+    alert("Please fill all values");
+  } else {
+    alert("Thank you for your response");
+    location.reload();
+  }
+}
